c95c378380cbba4bd2d1b2c41ec01b06c00319cd
furinax <gpfuri@hotmail.com>
2018-08-25 01:55:16 -0400

    seconds hand working

--- src/ClockFaceApp.cpp
+++ src/ClockFaceApp.cpp
@@ -1,6 +1,8 @@
 #include "cinder/app/App.h"
 #include "cinder/app/RendererGl.h"
 #include "cinder/gl/gl.h"
+#include "cinder/gl/Texture.h"
+#include "cinder/svg/Svg.h"
 
 using namespace ci;
 using namespace ci::app;
@@ -12,10 +14,16 @@ class ClockFaceApp : public App {
 	void mouseDown( MouseEvent event ) override;
 	void update() override;
 	void draw() override;
+
+	ci::ImageSourceRef imageSource;
+	gl::Texture2dRef mTexture;
 };
 
 void ClockFaceApp::setup()
 {
+	imageSource = loadImage(loadAsset("clock_face.png"));
+	mTexture = gl::Texture::create(imageSource);
+	setWindowSize(1000, 1000);
 }
 
 void ClockFaceApp::mouseDown( MouseEvent event )
@@ -28,7 +36,16 @@ void ClockFaceApp::update()
 
 void ClockFaceApp::draw()
 {
-	gl::clear( Color( 0, 0, 0 ) ); 
+	gl::clear( Color( 0, 1, 0 ) ); 
+	gl::pushModelView();
+
+	gl::translate(getWindowWidth()/2, getWindowHeight()/2);
+	gl::rotate(float(getElapsedSeconds() * 2.f * M_PI/60.f));
+	gl::translate(-250, -250);
+
+	gl::draw(mTexture);
+
+	gl::popModelView();
 }
 
 CINDER_APP( ClockFaceApp, RendererGl )
